dd77d40eb821b2de64ee51e13a4f22636105173b
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-30 14:54:10 +0300

    java problems

---
+++ 0x00-intro_to_java/JavaLoopsII.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class JavaLoopsII {
+    public static void main(String []arg){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int z = a;
+        for (int j = 0; j < n; j++) {
+            z += b * (1 << j);
+            System.out.print(z + " ");
+        }
+        System.out.println();
+        }
+        in.close();
+    }
+}
