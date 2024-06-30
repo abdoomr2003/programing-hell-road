f51b02eead3bbae50c52733ac860c7820942a429
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-30 14:54:58 +0300

    java problems

--- 0x00-intro_to_java/JavaLoopsII.java
+++ 0x00-intro_to_java/JavaLoopsII.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 public class JavaLoopsII {
     public static void main(String []arg){
--- 0x00-intro_to_java/Java_Datatypes.java
+++ 0x00-intro_to_java/Java_Datatypes.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 public class Java_Datatypes {
     public static void main(String []argh)
