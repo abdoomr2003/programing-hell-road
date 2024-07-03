d0443855156cc4f8ace8edaf361f51320978f530
abdoomr2003 <abdoomer1112003@gmail.com>
2024-07-03 10:36:06 +0300

    solve Ex. in Head first

--- second_week_Head_first/second_chapter/TapeDeckTestDrive.java
+++ second_week_Head_first/second_chapter/TapeDeckTestDrive.java
@@ -1,6 +1,6 @@
 class TapeDeckTestDrive {
     public static void main(String [] args) {
-        TapeDeck t = new TapeDeck();
+        TapeDeck t = new TapeDeck(); // this is line is added to solve
         t.canRecord = true;
         t.playTape();
         if (t.canRecord == true) {
